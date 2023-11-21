568b4050e56189036c6928eb1aaa9c54177a5144
George Arbanas <george.arbanas@icloud.com>
2023-11-21 16:09:44 +0900

    added projects to project page

---
+++ project.js
@@ -0,0 +1,42 @@
+const projects = document.querySelector(".projects");
+const arrows = document.querySelectorAll(".arrow");
+const projectBox = [];
+let count = 0;
+let activeProject;
+
+projects.childNodes.forEach((node) => {
+  if (node.nodeName === "LI") {
+    projectBox.push(node);
+  }
+});
+
+console.log(projectBox);
+
+function handleButton(event) {
+  console.log(event.target.id === "right-arrow" && count < projectBox.length);
+  if (event.target.id === "right-arrow" && count < projectBox.length) {
+    count += 1;
+    activeProject = projectBox[count];
+    displayProject(activeProject, "right", count);
+  } else if (event.target.id === "left-arrow" && count > 0) {
+    count -= 1;
+    activeProject = projectBox[count];
+    displayProject(activeProject, "left", count);
+  }
+}
+
+function displayProject(activeProject, direction, count) {
+  switch (direction) {
+    case "right":
+      projectBox[count - 1].style.display = "none";
+      break;
+    case "left":
+      projectBox[count + 1].style.display = "none";
+      break;
+  }
+  activeProject.style.display = "block";
+}
+
+arrows.forEach((arrow) => {
+  arrow.addEventListener("click", handleButton);
+});
